114d070281b05fdc8f00ef430451cc848e6e4af4
German Kuber <german.kuber@outlook.com>
2023-04-08 17:43:09 +0100

    First commit

---
+++ httpserver/src/main.rs
@@ -0,0 +1,13 @@
+mod handler;
+mod router;
+mod server;
+use server::Server;
+fn main() {
+    // Start a server
+    let server = Server::new("localhost:3000");
+    //Run the server
+    server.run();
+}
+
+
+- , and under   rout
